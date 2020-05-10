3e3b185456487db7f8772f5035d20fd917830a05
Oliver Herzig <Oliver.Herzig@montech.com>
2020-05-10 19:04:13 +0200

    implemented helper to avoid code duplication

---
+++ src/main/java/org/herzig/auction/control/helper/ViewHelper.java
@@ -0,0 +1,56 @@
+package org.herzig.auction.control.helper;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.herzig.auction.control.BaseController;
+import org.herzig.auction.model.Auction;
+
+import java.io.IOException;
+
+public class ViewHelper{
+    private View view;
+    private String windowTitle;
+    private Parent root;
+    private Stage stage;
+    private Object controller;
+
+    public static void showView(View view) throws IOException {
+        ViewHelper viewHelper = new ViewHelper(view);
+        viewHelper.showView();
+    }
+
+    public ViewHelper(View view, String windowTitle) throws IOException {
+        this.view = view;
+        this.windowTitle = windowTitle;
+        setupFXMLLoader();
+
+    }
+
+    public ViewHelper(View view) throws IOException {
+        this(view, view.getViewTitle());
+    }
+
+    public void showView() throws IOException {
+        setupStage();
+        this.stage.show();
+    }
+
+    public Object getController() {
+        return this.controller;
+    }
+
+    private void setupFXMLLoader() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(this.view.getViewPath()));
+        this.root = loader.load();
+        this.controller = loader.getController();
+    }
+
+    private void setupStage() {
+        this.stage = new Stage();
+        this.stage.setScene(new Scene(this.root));
+        this.stage.setTitle(this.windowTitle);
+    }
+
+}
