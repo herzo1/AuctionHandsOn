7effbf5f24526ce5e3c5211b75273eb11ace8644
Oliver Herzig <Oliver.Herzig@montech.com>
2020-05-10 15:48:17 +0200

    fully implemented administration controller

--- src/main/java/org/herzig/auction/control/AdministrationController.java
+++ src/main/java/org/herzig/auction/control/AdministrationController.java
@@ -1,28 +1,63 @@
 package org.herzig.auction.control;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+import org.herzig.auction.model.Auction;
+import org.herzig.auction.model.AuctionObserver;
+import org.herzig.auction.model.AuctionSystem;
+import org.herzig.auction.model.Item;
 
-public class AdministrationController {
+import java.io.IOException;
+import java.util.List;
+
+public class AdministrationController implements AuctionObserver {
     @FXML
-    private ListView<Object> items;
+    private ListView<Item> items;
 
     @FXML
     private Button startAuctionBtn;
 
+    public void initialize() {
+        List<Item> list = AuctionSystem.getInstance().getCatalogue().getItems();
+        this.items.getItems().addAll(list);
+        this.items.getSelectionModel().clearAndSelect(0);
+    }
+
     @FXML
-    public void handleStartAuction() {
-        System.out.println("Start Auction");
+    public void handleStartAuction() throws IOException {
+        this.startAuctionBtn.setDisable(true);
+        Item item = this.items.getSelectionModel().getSelectedItem();
+        Auction auction = new Auction(item);
+        auction.addObserver(this);
+
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/AuctionView.fxml"));
+        Parent root = loader.load();
+        ((AuctionController)loader.getController()).setAuction(auction);
+
+        stage.setScene(new Scene(root));
+        stage.setTitle("Auction");
+        stage.show();
+
+        auction.start();
     }
 
     @FXML
     public void handleShowBids() {
-        System.out.println("Show Bids");
-    }
-
-    public void initialize() {
         // todo
     }
 
+    @Override
+    public void update(Auction auction) {
+        if(auction.getStatus() == Auction.Status.TERMINATED) {
+            this.startAuctionBtn.setDisable(false);
+            auction.removeObserver(this);
+        }
+
+    }
 }
