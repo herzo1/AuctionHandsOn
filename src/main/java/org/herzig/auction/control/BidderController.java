bcae22dc25cf50756bbc6ef8d9fc7aa87b280a10
Oliver Herzig <Oliver.Herzig@montech.com>
2020-05-10 20:07:35 +0200

    refactored bidder controller

--- src/main/java/org/herzig/auction/control/BidderController.java
+++ src/main/java/org/herzig/auction/control/BidderController.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.CheckBoxListCell;
 import org.herzig.auction.model.*;
 import org.herzig.auction.model.robot.Eager;
 import org.herzig.auction.model.robot.Robot;
@@ -49,19 +50,14 @@ public class BidderController extends BaseController {
     Button stopBtn;
 
     public void initialize() {
-        List<String> list = new ArrayList<>();
-        for(RobotBidder robot : RobotBidder.values()){
-            list.add(robot.getName());
-        }
-        this.strategyCB.setItems(FXCollections.observableArrayList(list));
-        this.strategyCB.getSelectionModel().select(0);
-        this.stopBtn.setDisable(true);
+        fillStrategyChoiceBoxAndSetDefault();
+        disableRobotButton(false, true);
     }
 
     public void setAuction(Auction auction) {
         this.auction = auction;
         this.auction.addObserver(this);
-        this.itemLbl.setText(auction.getItem().toString());
+        setItemLabel(auction);
         updateCurrentBidLbl();
     }
 
@@ -72,15 +68,10 @@ public class BidderController extends BaseController {
     @FXML
     public void placeBid() {
         this.messageLbl.setText("");
-        String amountStr = this.amountTF.getText();
-        if(!amountStr.matches("\\d+(\\.\\d+)?")) {
-            this.messageLbl.setText("Amount must be a positive number.");
-            return;
-        }
-        double amount = Double.parseDouble(amountStr);
-        Bid bid = new Bid(this.bidder, amount);
-        try{
-            this.auction.placeBid(bid);
+        try {
+            this.auction.placeBid(new Bid(
+                    this.bidder,
+                    getAmountFromAmountTF()));
         } catch (InvalidBidException e) {
             this.messageLbl.setText(e.getMessage());
         }
@@ -88,22 +79,15 @@ public class BidderController extends BaseController {
 
     @FXML
     public void startRobot() {
-        Strategy strategy = Objects.requireNonNull(RobotBidder.getRobotBidder(this.strategyCB.getSelectionModel()
-                .getSelectedItem()))
-                .getStrategy();
-        this.robot = new Robot(this.bidder, this.auction, strategy);
-        this.robot.start();
-        this.startBtn.setDisable(true);
-        this.stopBtn.setDisable(false);
+        setupRobot();
+        disableRobotButton(true, false);
     }
 
     @FXML
     public void stopRobot() {
-        if (this.robot != null) {
-            this.robot.stop();
-            this.robot = null;
-            this.startBtn.setDisable(false);
-            this.stopBtn.setDisable(true);
+        if (hasRobot()) {
+            terminateRobot();
+            disableRobotButton(false, true);
         }
     }
 
@@ -112,14 +96,63 @@ public class BidderController extends BaseController {
         if(auction.isRunning()) {
             updateCurrentBidLbl();
         } else if(auction.isTerminated()) {
-            this.placeBidBtn.setDisable(true);
-            if(this.robot != null) {
-                this.robot.stop();
-                this.robot = null;
-            }
-            this.startBtn.setDisable(true);
-            this.stopBtn.setDisable(true);
+            updateUIForTerminatedStatus();
         }
+    }
+
+    private void fillStrategyChoiceBoxAndSetDefault() {
+        this.strategyCB.setItems(FXCollections.observableArrayList(getStrategyList()));
+        this.strategyCB.getSelectionModel().select(0);
+    }
+
+    private List<String> getStrategyList(){
+        List<String> list = new ArrayList<>();
+        for(RobotBidder robot : RobotBidder.values()){
+            list.add(robot.getName());
+        }
+        return list;
+    }
+
+    private void updateUIForTerminatedStatus() {
+        this.placeBidBtn.setDisable(true);
+        if(hasRobot()) {
+            terminateRobot();
+        }
+        disableRobotButton(true, true);
+    }
+
+    private void setItemLabel(Auction auction) {
+        this.itemLbl.setText(auction.getItem().toString());
+    }
+
+    private void disableRobotButton(boolean disableStartBtn, boolean disableStopBtn){
+        this.startBtn.setDisable(disableStartBtn);
+        this.stopBtn.setDisable(disableStopBtn);
+    }
+
+    private double getAmountFromAmountTF() throws InvalidBidException {
+        String amountStr = this.amountTF.getText();
+        if(!amountStr.matches("\\d+(\\.\\d+)?")) {
+            throw new InvalidBidException("Amount must be a positive number.");
+        }
+        return Double.parseDouble(amountStr);
+    }
+
+    private void setupRobot() {
+        Strategy strategy = Objects.requireNonNull(RobotBidder.getRobotBidder(this.strategyCB.getSelectionModel()
+                .getSelectedItem()))
+                .getStrategy();
+        this.robot = new Robot(this.bidder, this.auction, strategy);
+        this.robot.start();
+    }
+
+    private boolean hasRobot() {
+        return this.robot != null;
+    }
+
+    private void terminateRobot() {
+        this.robot.stop();
+        this.robot = null;
     }
 
     private void updateCurrentBidLbl() {
