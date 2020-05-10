4f5c1088d7e004da3fb269614bbf21946b29e967
Oliver Herzig <Oliver.Herzig@montech.com>
2020-05-10 14:47:33 +0200

    implemented BaseController

---
+++ src/main/java/org/herzig/auction/control/BaseController.java
@@ -0,0 +1,15 @@
+package org.herzig.auction.control;
+
+import javafx.application.Platform;
+import org.herzig.auction.model.Auction;
+import org.herzig.auction.model.AuctionObserver;
+
+public abstract class BaseController implements AuctionObserver {
+
+    @Override
+    public void update(Auction auction) {
+        Platform.runLater(() -> handleUpdate(auction));
+    }
+
+    abstract void handleUpdate(Auction auction);
+}
