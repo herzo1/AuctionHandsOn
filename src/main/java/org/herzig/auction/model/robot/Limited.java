ffa3750c39a205900c9a3f767116c3183686accd
Oliver Herzig <Oliver.Herzig@montech.com>
2020-05-10 19:37:51 +0200

    created limited strategy

---
+++ src/main/java/org/herzig/auction/model/robot/Limited.java
@@ -0,0 +1,11 @@
+package org.herzig.auction.model.robot;
+
+import org.herzig.auction.model.Bid;
+import org.herzig.auction.model.User;
+
+public class Limited extends Strategy {
+    @Override
+    Bid createBid(User bidder, double amount) {
+        return null;
+    }
+}
